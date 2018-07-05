40cd31932302c0b68aa7df9784baeb066d184d88
roman <gooood85@yandex.ru>
2018-07-05 20:10:33 +0500

    Отпимизация Cesium за счет уменьшения количества сканирований (избыточных) положения указателя мыши в мировом пространстве.

--- Source/Renderer/DrawCommand.js
+++ Source/Renderer/DrawCommand.js
@@ -559,7 +559,7 @@ import PrimitiveType from '../Core/PrimitiveType.js';
         const v = this.vertexArray;
         if(!v || !v.isDestroyed()) //TODO после сортировки CESIUM_3D_TILE стали попадаться битые команды
             context.draw(this, passState);
-        else
-            console.warn("destroyed command");
+//        else
+//            console.warn("destroyed command");
     };
 export default DrawCommand;
