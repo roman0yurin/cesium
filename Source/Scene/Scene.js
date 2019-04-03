a4d0c338e904a34dcde38dc1ede373b3c4008a08
roman0yurin <gooood85@yandex.ru>
2019-04-03 16:39:14 +0500

    Возможность скрывать объекты на 3д сцене. Рефакторинг GUI карты.

--- Source/Scene/Scene.js
+++ Source/Scene/Scene.js
@@ -3253,6 +3253,12 @@ define([
         }
     }
 
+    /**Нужно в данный момент рендерить следующий кадр.**/
+    Scene.prototype.shouldRenderFrame = function(){
+        var cameraChanged = this._view.checkForCameraUpdates(this);
+        return !this.requestRenderMode || this._renderRequested || cameraChanged || this._logDepthBufferDirty || this._hdrDirty || (this.mode === SceneMode.MORPHING);
+    }
+
     /**
      * Update and render the scene.
      * @param {JulianDate} [time] The simulation time at which to render.
@@ -3267,8 +3273,8 @@ define([
         var frameState = this._frameState;
         this._jobScheduler.resetBudgets();
 
-        var cameraChanged = this._view.checkForCameraUpdates(this);
-        var shouldRender = !this.requestRenderMode || this._renderRequested || cameraChanged || this._logDepthBufferDirty || this._hdrDirty || (this.mode === SceneMode.MORPHING);
+        var shouldRender = this.shouldRenderFrame();
+
         if (!shouldRender && defined(this.maximumRenderTimeChange) && defined(this._lastRenderTime)) {
             var difference = Math.abs(JulianDate.secondsDifference(this._lastRenderTime, time));
             shouldRender = shouldRender || difference > this.maximumRenderTimeChange;
